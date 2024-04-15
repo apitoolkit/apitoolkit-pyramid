24732ac194fd28da421b7272a4ef21897d215b60
dawkaka <yousiph77@gmail.com>
2024-04-15 10:55:45 +0000

    publish new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="apitoolkit-pyramid",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     description='A Python Pyramid SDK for Apitoolkit integration',
     long_description=long_description,
